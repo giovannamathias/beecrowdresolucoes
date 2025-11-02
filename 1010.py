de763fb39b0786b8fb390b69ccf0c2f120cb9c0e
Giovanna Mathias <giovannamathias06@gmail.com>
2025-11-02 16:43:55 -0300

    Update 1010.py

--- 1010.py
+++ 1010.py
@@ -6,3 +6,13 @@ peca2 = input().split()
 cod1 = int(peca1[0])
 quant1 = int(peca1[1])
 valor1 = float(peca1[2])
+
+cod2 = int(peca2[0])
+quant2 = int(peca2[1])
+valor2 = float(peca2[2])
+
+valor_peca1 = quant1 * valor1
+valor_peca2 = quant2 * valor2
+valor_total = valor_peca1 + valor_peca2
+
+print(f'VALOR A PAGAR: R$ {valor_total:.2f}')
