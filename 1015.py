11446c09a478c6d3eac7564ba9580ecaeb980dc3
Giovanna Mathias <giovannamathias06@gmail.com>
2025-11-02 16:47:20 -0300

    Desafio 1015
    Desafio 1015 Beecrowd - Distância entre dois pontos em Python.

---
+++ 1015.py
@@ -0,0 +1,9 @@
+#1015 Distância entre dois pontos
+valores1 = input().split()
+valores2 = input().split()
+x1 = float(valores1[0])
+y1 = float(valores1[1])
+x2 = float(valores2[0])
+y2 = float(valores2[1])
+distancia = (((x2-x1)**2) + ((y2 - y1)**2))**0.5
+print(f'{distancia:.4f}')
